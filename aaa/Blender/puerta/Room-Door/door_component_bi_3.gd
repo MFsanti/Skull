655b9c35c11b93b002633176e11a29392d097057
alumnoing <santiagovalentinguajardo@gmail.com>
2024-09-27 11:56:24 -0300

    agrego escaleras y comodas

---
+++ aaa/Blender/puerta/Room-Door/door_component_bi_3.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+var isOen = false
+var usable = true
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func action_use():
+	print("hola")
---
+++ aaa/codes/puerta.gd
@@ -0,0 +1,27 @@
+extends Node3D
+
+var isOpen = false
+var usable = true
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func action_use():
+	if ! isOpen and usable: 
+		open_door()
+	elif isOpen and usable: 
+		close_door()
+
+func open_door(): 
+	usable = false 
+	$AnimationPlayer.play("open")
+	isOpen = true
+ 
+func close_door(): 
+	usable = false 
+	$AnimationPlayer.play("close")
+	isOpen = false
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	usable = true
