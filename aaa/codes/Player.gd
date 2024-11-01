fb2deaf4bf3c11a60e1e9f4a2901137192c115a2
alumnoing <santiagovalentinguajardo@gmail.com>
2024-11-01 11:54:48 -0300

    funcion pick a object

--- aaa/codes/Player.gd
+++ aaa/codes/Player.gd
@@ -1,34 +1,32 @@
-
 extends CharacterBody3D
 
 const WALK_SPEED = 0.7
-
-const GRAVITY = -9.8  # Gravedad más realista
-
-const SLOW_DOWN = 0.5  # Velocidad de descenso
-
+const GRAVITY = -9.8
 const SENSITIVITY = 0.005
 
 @onready var head = $Head
-
 @onready var camera = $Head/Camera3D
-
 @onready var raycast = $Head/Camera3D/RayCast3D
-
 @onready var hint_icon = %ActionHintIcon
+@onready var point = %point
+
+var dragged_item = null
+var cursor_offset = Vector3(0, 1, 0)
+var agarrado: RigidBody3D
+ # Ajusta la altura del objeto arrastrado
 
 func _physics_process(delta: float) -> void:
-		if not is_on_floor():
-			velocity.y += GRAVITY * delta
-		else:
-			velocity.y = 0
-		move_and_slide()
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+	else:
+		velocity.y = 0
+	move_and_slide()
 
 func _process(delta: float) -> void:
 	process_raycast_cool()
 
+	# Movimiento del jugador
 	var speed = WALK_SPEED
-
 	if Input.is_action_pressed("right"):
 		position += head.transform.basis.x * delta * speed
 	if Input.is_action_pressed("left"):
@@ -40,38 +38,37 @@ func _process(delta: float) -> void:
 
 	move_and_slide()
 
-func _ready() -> void: 
-
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func process_raycast_cool():
 	hint_icon.hide()
 	if raycast.is_colliding(): 
 		var collider = raycast.get_collider()
-		#print("Colisiona con: ", collider.name)  # Para verificar con qué colisiona
 		if collider.has_method("action_use"):
 			if Input.is_action_just_pressed("usar"):
-				collider.action_use()
+				collider.action_use()  # Llama al método de recogida
 			else:
 				hint_icon.show()
-				#print("MOstrar icono de accion")
-		#else:
-			#print("El objeto no tiene el método action_use")
-	#else:
-		#print("No colisiona con nada")
-		
-func process_raycast():
-	if raycast.is_colliding(): 
-		var collider = raycast.get_collider()
-		if collider.has_method("action_use"):
-			collider.action_use()
-		#else:
-			#print("El objeto no tiene el método action_use")
-	#else:
-		#print("No colisiona con nada")
-
+		if agarrado == null and collider.is_in_group("agarrables"):
+			if Input.is_action_just_pressed("agarrar"):
+				agarrar_objeto(collider)
+		if agarrado != null and Input.is_action_just_released("agarrar"):
+			soltar_objeto()
+				
+				
+func agarrar_objeto(objeto):
+	agarrado = objeto
+	agarrado.freeze = true
+	
+	agarrado.reparent(point)
+	agarrado.position = Vector3.ZERO
+func soltar_objeto():
+	agarrado.reparent(get_parent())
+	agarrado.freeze = false
+	agarrado = null
 
 func _input(event):
 	if event is InputEventMouseMotion: 
 		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY) 
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
